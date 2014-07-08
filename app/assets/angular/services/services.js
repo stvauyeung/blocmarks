169a06a431fa7e0f16bd87961364a834944236b6
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-07-07 22:11:13 -0700

    updated CurrentUser get

--- app/assets/angular/services/services.js
+++ app/assets/angular/services/services.js
@@ -3,11 +3,10 @@ app = angular.module('blocMarksServices', []);
 app.service('CurrentUser', ['$http', function($http) {
   return {
     get: function(userId) {
-      $http({
-        url: '/api/users/:id',
-        method: "GET",
-        params: {id: userId}
-      });
+      $http.get('api/users/' + userId)
+        .success(function(data, status, headers) {
+          console.log("success");
+        });
     }
   }
 }]);
