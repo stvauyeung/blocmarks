e0f5ef259003cc8922375206b21c56ef4e6b41b9
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-07-08 11:38:33 -0700

    tweaking service

--- app/assets/angular/services/services.js
+++ app/assets/angular/services/services.js
@@ -3,9 +3,12 @@ app = angular.module('blocMarksServices', []);
 app.service('CurrentUser', ['$http', function($http) {
   return {
     get: function(userId) {
-      $http.get('api/users/' + userId)
+      $http.get('/api/users/' + userId + '.json')
         .success(function(data, status, headers) {
-          console.log("success");
+          return data;
+        })
+        .erro(function(data, status, headers) {
+          console.log('error');
         });
     }
   }
