09f74e04b11d6628addb47b812324e6cb8514a6b
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-07-23 09:12:42 -0700

    created votecount directive

--- app/assets/angular/models/bookmark.js
+++ app/assets/angular/models/bookmark.js
@@ -9,6 +9,14 @@ model.factory('Bookmark', ['$http', '$window', function($http, $window) {
         }).error(function() {
           console.log('error with bookmark creation');
         });
-    } 
+    },
+    show: function(bookmarkId) {
+      return $http.get('/api/bookmarks/' + bookmarkId)
+        .success(function(data) {
+          return data;
+        }).error(function(data) {
+          console.log('Error showing bookmark');
+        })
+    }
   }
 }]);
