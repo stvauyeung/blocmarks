ae3a61c03128c287317f8cfa6ea17f4d59c80b09
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-07-22 21:21:48 -0700

    fix navbar modal

--- app/assets/angular/models/bookmark.js
+++ app/assets/angular/models/bookmark.js
@@ -3,7 +3,7 @@ model = angular.module('bookmarkModel', []);
 model.factory('Bookmark', ['$http', '$window', function($http, $window) {
   return {
     create: function(bookmark) {
-      $http.post('api/bookmarks', bookmark)
+      $http.post('/api/bookmarks', bookmark)
         .success(function(data) {
           $window.location.href = '/home';
         }).error(function() {
